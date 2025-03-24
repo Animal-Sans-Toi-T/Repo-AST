695de301cc8cbce1f757f0baef4fad20039d280e
Fredf <fredfernandez488@gmail.com>
2025-03-24 10:34:59 +0100

    push de master sur dev, début de projet

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,16 +5,19 @@ import "../style/Header.css"
 const Header = () => {
     return (
         <header>
+            <nav>
+                <ul>
+                    <li><a href="">ASBL</a></li>
+                    <li><a href="">CONTACT</a></li>
+                    <li><a href="">ADOPTABLES</a></li>
+                    <li><a href="">GALERIE</a></li>
+                    <li><a href="">NOUS AIDER</a></li>  
+                </ul>
+                <button>SIGNALER</button>
+            </nav>
             <div>
                 <h1>Refuge</h1>
             </div>
-            <nav>
-                <ul>
-                    <li><a href="">Acceuil</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">A propos</a></li>
-                </ul>
-            </nav>
         </header>
     );
 };
