7d43210819ad6471dc9917e6543c45a39a10ef7b
Fredf <fredfernandez488@gmail.com>
2025-03-24 16:24:32 +0100

    Début de réalisation du header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import "../style/Header.css"
-
+import logo from "../assets/logo-ast.png"
 
 const Header = () => {
     return (
         <header>
-            <nav>
-                <ul>
-                    <li><a href="">ASBL</a></li>
-                    <li><a href="">CONTACT</a></li>
-                    <li><a href="">ADOPTABLES</a></li>
-                    <li><a href="">GALERIE</a></li>
-                    <li><a href="">NOUS AIDER</a></li>  
-                </ul>
-                <button>SIGNALER</button>
-            </nav>
-            <div>
-                <h1>Refuge</h1>
+            <div className="header-container">
+                <img src={logo} alt="logo" className="header-logo" />
+                <nav>
+                    <ul>
+                        <div className="header-nav-bar">
+                            <li><a href="">ASBL</a></li>
+                            <li><a href="">CONTACT</a></li>
+                            <li><a href="">ADOPTABLES</a></li>
+                            <li><a href="">GALERIE</a></li>
+                            <li><a href="">NOUS AIDER</a></li> 
+                            <li><a className="signaler" href="">SIGNALER</a></li>
+                        </div> 
+                    </ul>
+                </nav>
             </div>
         </header>
     );
