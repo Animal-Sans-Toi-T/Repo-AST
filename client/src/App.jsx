44eeaa75b47ecf8a128d376ec3ae21c0841b8743
Emi <emilieprick@gmail.com>
2025-03-27 17:14:50 +0100

    Banner - filter autoplay ok but still need animation for autoplay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './style/App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Banner from './components/Banner';
 import Caroussel from './components/Caroussel';
 import DonationsButton from './components/DonationsButton';
 import PresentationCard from './components/PresentationCard';
@@ -18,7 +19,7 @@ function App() {
     <div className='mainContainer'>
       <Header />
       <main>
-        <h2>Page d'acceuil</h2>
+        <Banner/>
         <Caroussel images={images} />
         <PresentationCard/>
         <DonationsButton />
