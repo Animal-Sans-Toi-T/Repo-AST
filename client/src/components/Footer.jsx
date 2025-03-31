7cd7bde774d9d0fa555e0d65bec1fbb7b39ad82c
Marie Tomor <marietomorpro@gmail.com>
2025-03-31 16:58:35 +0200

    Creation footer et ajout du responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,75 @@
 import React from "react";
-import "../style/Footer.css"
+import "../style/Footer.css";
+import iconPlace from "../assets/icon-place.png";
+import iconPhone from "../assets/icon-phone.png";
+import iconMail from "../assets/icon-mail.png";
+import iconMedia from "../assets/icon-media.png";
+import iconTime from "../assets/icon-time.png";
+import MapGoogle from "./MobileMapGoogle.jsx";
+import MainMap from "./MainMapGoogle.jsx";
+import MobileMap from "./MobileMapGoogle.jsx";
+import HoverImage from "../assets/img-hover.jpg";
 
 const Footer = () => {
     return (
         <footer>
-            <p>Copyright, tout droit réservé</p>
-            <ul>
-                <li>Coordonnées</li>
-                <li>Horaires</li>
-            </ul>
+            <div className="footer-container">
+                
+                {/* COORDONNEES */}
+                <nav className="container-icon-coordonnees">
+                    <div className="div-place">
+                        <a className="a-icon-place" href=""><img className="img-icon iconPlace" src={iconPlace} alt="Icon Place" /></a>
+                        <a className="link-map" href="https://maps.app.goo.gl/oJSxgZzXtuabdbPz5">Rue de Huy 188, 4317 Faimes</a>
+                        <div className="container-mobile-map-google">
+                            < MobileMap/>
+                        </div>
+                    </div>
+                        
+                    <div className="div-phone">
+                        <img className="img-icon iconPhone" src={iconPhone} alt="Icon Phone" />
+                        <p>0478 572 660</p>
+                    </div>
+
+                    <div className="div-mail">
+                        <a className="a-icon-mail" href=""><img className="img-icon iconMail" src={iconMail} alt="Icon e-mail" /></a>
+                        <p>Contactez-nous</p>
+                    </div>
+
+                    <div className="div-media">
+                        <a className="a-icon-media" href=""><img className="img-icon iconMedia" src={iconMedia} alt="Icon Media" /></a>
+                        <p>Restons connectés</p>
+                    </div>
+                </nav>
+
+                {/* EMPTY */}
+                <div className="empy">
+                < MainMap/>
+                </div>
+
+                {/* HORAIRES */}
+                <hr className="hr-mobile" />
+                <div className="div-horaires">
+                    <img className="img-icon iconTime" src={iconTime} alt="Horaires" />
+                    <ul className="subtext-horaires">
+                        <li>14H - 17H</li>
+                        <li>tous les jours</li>
+                    </ul>
+                </div>
+                <hr className="hr-mobile" />
+
+                {/* COPYRIGHT */}
+                <div className="div-text-copyright">
+                    <div className="div-infos">
+                        <p className="p-infos-1">Siège Social : 188 rue de Huy, 4317 Viemme (Faimes) | 019 63 00 36</p>
+                        <p className="p-infos-2">N° entreprise : 0478 572 660 | N° agrément : HK 306 03 305 | Iban : BE24 0019 9777 7038 | Bic : GEBA BEBB</p>
+                    </div>
+                    <div className="hover-container">
+                        <p className="copyright">&#169; Copyright, tous droits réservés</p>
+                        <img className="hover-image" src={HoverImage} alt="Image surprise" />
+                    </div>
+                </div>
+                
+            </div>
         </footer>
     );
 };
---
+++ client/src/components/MainMapGoogle.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MainMap = () => {
+return (
+<div>
+    <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2407.9103653544666!2d5.267472299999999!3d50.64962489999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c1041b4be4b731%3A0x1e8aac14195e5e44!2sAnimal%20Sans%20Toit!5e1!3m2!1sfr!2sbe!4v1743161047076!5m2!1sfr!2sbe"
+    width="400"
+    height="200"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+    title="Carte"
+    ></iframe>
+</div>
+);
+};
+
+export default MainMap;
