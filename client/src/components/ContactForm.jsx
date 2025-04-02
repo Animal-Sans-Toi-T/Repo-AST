a7d286a893186f4612494b60ac0703c914312198
Emi <emilieprick@gmail.com>
2025-04-02 20:16:17 +0200

    ContactPage - CSS, adding elements to html, prepping the setFormInput

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -6,22 +6,27 @@ import RGPD from './RGPD.jsx';
 
 function ContactForm() {
 
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastname] = useState("");
-    const [street, setStreet] = useState("");
-    const [zipcode, setZipcode] = useState("");
-    const [city, setCity] = useState("");
-    const [country, setCountry] = useState("");
-    const [phone, setPhone] = useState("");
-    const [email, setEmail] = useState("");
-    const [inquiry, setInquiry] = useState("");
-    const [subject, setSubject] = useState("");
+    // const [firstname, setFirstname] = useState("");
+    // const [lastname, setLastname] = useState("");
+    // const [street, setStreet] = useState("");
+    // const [zipcode, setZipcode] = useState("");
+    // const [city, setCity] = useState("");
+    // const [country, setCountry] = useState("");
+    // const [phone, setPhone] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [inquiry, setInquiry] = useState("");
+    // const [subject, setSubject] = useState("");
 
     const [formInputs, setFormInputs] = useState({
         lastName: '',
         firstName: '',
-        generalSubject: '',
-
+        street: '',
+        zipCode:'',
+        city:'',
+        country:'',
+        phone:'',
+        email:'',
+        inquiry:''
     });
 
 
@@ -32,6 +37,40 @@ function ContactForm() {
     const handleFirstnameChange = (e) => {
         setFormInputs({ ...formInputs, firstName: e.target.value })
     };
+
+    const handleStreetChange = (e) => {
+        setFormInputs({ ...formInputs, street: e.target.value })
+    };
+
+    const handleZipCodeChange = (e) => {
+        setFormInputs({ ...formInputs, zipCode: e.target.value })
+    };
+
+    const handleCityChange = (e) => {
+        setFormInputs({ ...formInputs, city: e.target.value })
+    };
+    
+
+    const handleCountryChange = (e) => {
+        setFormInputs({ ...formInputs, country: e.target.value })
+    };
+    
+
+    const handlePhoneChange = (e) => {
+        setFormInputs({ ...formInputs, phone: e.target.value })
+    };
+    
+
+    const handleEmailChange = (e) => {
+        setFormInputs({ ...formInputs, email: e.target.value })
+    };
+    
+
+    const handleInquiryChange = (e) => {
+        setFormInputs({ ...formInputs, inquiry: e.target.value })
+    };
+    
+    
 
     const submitForm = (formInputs) => {
         setFormInputs(formInputs);
@@ -84,45 +123,70 @@ function ContactForm() {
         <>
             <form className='contactForm' action="mailto:emilieprick@gmail.com" method="post" enctype="text/plain">
 
+
+{/* EMI: à mon avis tu devras mettre l'adresse email dans value; après à voir si par exemple la personne change son choix, si la valeur n'est pas encodée de toute façon dans le setFormInput... a mon avis il faudra faire une logique de true/false avec des if pour dtminer où envoyer */}
                 <div className='radioButtonsContainer'>
-                    <h2>Je souhaite:</h2>
+                    <h3>Ma demande concerne:</h3>
                     <div className='radioButtonsBlock'>
                         <input className='radioButton' type="radio" name="generalSubject" id="général" onChange={(e) => handleSubject(e,)} value={false} />
-                        <label for="plaintes">Demander une information générale</label></div>
-                    <div className='radioButtonsBlock'><input className="radioButton" type="radio" name="subject" id="maltraitance" value="plaintes" />
-                        <label for="plaintes">Dénoncer une maltraitance</label></div>
-                    <div className='radioButtonsBlock'><input className="radioButton" type="radio" name="subject" id="présidente" value="plaintes" />
-                        <label for="plaintes">Contacter la présidente</label></div>
+                        <label for="plaintes">Une information générale</label>
+                    </div>
+                    <div className='radioButtonsBlock'>
+                        <input className="radioButton" type="radio" name="subject" id="maltraitance" value="plaintes" />
+                        <label for="plaintes">Une maltraitance</label>
+                    </div>
+                    <div className='radioButtonsBlock'>
+                        <input className="radioButton" type="radio" name="subject" id="présidente" value="plaintes" />
+                        <label for="plaintes">Contact avec la présidente</label>
+                    </div>
+                    <div className='radioButtonsBlock'>
+                        <input className='radioButton' type="radio" name="generalSubject" id="général" onChange={(e) => handleSubject(e,)} value={false} />
+                        <label for="plaintes">Le parrainage</label>
+                    </div>
+                    <div className='radioButtonsBlock'>
+                        <input className='radioButton' type="radio" name="generalSubject" id="général" onChange={(e) => handleSubject(e,)} value={false} />
+                        <label for="plaintes">Le service "membres"</label>
+                    </div>
+                    <div className='radioButtonsBlock'>
+                        <input className='radioButton' type="radio" name="generalSubject" id="général" onChange={(e) => handleSubject(e,)} value={false} />
+                        <label for="plaintes">Le bénévolat</label>
+                    </div>
+                    <div className='radioButtonsBlock'>
+                        <input className='radioButton' type="radio" name="generalSubject" id="général" onChange={(e) => handleSubject(e,)} value={false} />
+                        <label for="plaintes">Le site internet</label>
+                    </div>
                 </div>
 
                 <div className='textFormsContainer'>
                     <label>Nom*:<input type="text" name="lastName" placeholder="Nom *" onChange={(e) => handleLastnameChange(e)} required /></label>
                     <label>Prénom*:<input type="text" name="firstName" placeholder='Prénom *' onChange={(e) => handleFirstnameChange(e)} required /></label>
-                    {/* <label>Rue:
-                        <input type="text" name="street" placeholder='Rue' required /></label>
+                    <label>Rue:
+                        <input type="text" name="street" placeholder='Rue' onChange={(e) => handleStreetChange(e)} required/></label>
                     <label>Code postal:
-                        <input type="number" name="zipCode" placeholder='Code Postal' required /></label>
+                        <input type="number" name="zipCode" placeholder='Code Postal' onChange={(e) => handleZipCodeChange(e)}required /></label>
                     <label>Localité:
-                        <input type="text" name="city" placeholder='Localité' required /></label>
+                        <input type="text" name="city" placeholder='Localité' onChange={(e) => handleCityChange(e)} required /></label>
                     <label>Pays:
-                        <input type="text" name="country" placeholder='Pays' required /></label>
+                        <input type="text" name="country" placeholder='Pays' onChange={(e) => handleCountryChange(e)} required /></label>
                     <label>Téléphone:
-                        <input type="tel" name="phone" placeholder='Numéro de téléphone' required /></label>
+                        <input type="tel" name="phone" placeholder='Numéro de téléphone' onChange={(e) => handlePhoneChange(e)} required /></label>
                     <label>Email*:
-                        <input type="email" name="email" placeholder='Email' required /></label>
-                    <label>Demande*:
+                        <input type="email" name="email" placeholder='Email' onChange={(e) => handleEmailChange(e)} required /></label>
+                    <label>Demande*: <br/>
                         <textarea type="textarea" placeholder='Votre demande/question' minlength="4"
-                            maxlength="250" size="50" name="inquiry" required /></label> */}
+                            maxlength="250" size="50" name="inquiry" onChange={(e) => handleInquiryChange(e)} required /></label>
                 </div>
+
                 <div className='checkNewsletterBlock'>
                     <input className='checkboxNewsletter' type="checkbox" id="newsletter" name="newsletter" />
                     <label for="newsletter">Je souhaite recevoir les newsletters</label>
                 </div>
+
                 <div className='RGPD'>
-                    <input className='checkboxRGPD'type="checkbox" id="RGPD" name="RGPD" required />
-                    <label for="RGPD">J'accepte les RGPD</label>
+                    <input className='checkboxRGPD' type="checkbox" id="RGPD" name="RGPD" required />
+                    <label for="RGPD">J'accepte les RGPD  <RGPD /></label>
                 </div>
-                <RGPD />
+
 
                 <button className="send" type="submit" onSubmit={() => submitForm(formInputs)}>Envoyer</button>
 
